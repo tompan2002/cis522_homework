ac6046f61646c353f4d5055a6e6083579b5e65f8
Tom <tom@lev-03350.apn.wlan.private.upenn.edu>
2023-03-18 20:27:47 -0400

    v2

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,11 +6,11 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 64
+    batch_size = 256
     num_epochs = 8
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-2)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
     transforms = Compose([ToTensor()])
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -3,25 +3,47 @@ import torch.nn as nn
 
 
 class Model(torch.nn.Module):
+    """
+    CNN model
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
+        """
+        Initializes layers of model
+
+        Arguments:
+            num_channels (int): Number of channels of input, num_classes (int): Number of possible classes.
+
+        Returns:
+            Nothing.
+        """
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(
-                in_channels=num_channels, out_channels=9, kernel_size=3, padding=1
+                in_channels=num_channels, out_channels=12, kernel_size=3, padding=1
             ),
             nn.ReLU(),
-            nn.BatchNorm2d(9),
+            nn.BatchNorm2d(12),
             nn.MaxPool2d(kernel_size=3, stride=3, padding=1),
-            nn.Conv2d(in_channels=9, out_channels=18, kernel_size=2, padding=1),
+            nn.Conv2d(in_channels=12, out_channels=21, kernel_size=2, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(18),
+            nn.BatchNorm2d(21),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
             nn.Flatten(),
         )
 
-        self.classifier = nn.Sequential(nn.Linear(7 * 7 * 18, num_classes))
+        self.classifier = nn.Sequential(nn.Linear(7 * 7 * 21, num_classes))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Passes inputs through model and returns probabilities of classes
+
+        Arguments:
+            x (torch.Tensor): Image to classify
+
+        Returns:
+            Tensor of probabilities of each class for the image
+        """
         x = self.features(x)
         x = self.classifier(x)
         return x
