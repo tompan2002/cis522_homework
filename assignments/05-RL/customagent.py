e2b74636b74b18e4d98af5a8f0c28f4e0ad491b0
Tom <tom@lev-08863.apn.wlan.private.upenn.edu>
2023-04-26 22:26:06 -0400

    hw5 v7

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -16,9 +16,10 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.fc1 = nn.Linear(observation_space, 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.fc3 = nn.Linear(32, 32)
-        self.fc7 = nn.Linear(32, action_space)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc7 = nn.Linear(64, action_space)
 
     def forward(self, x):
         """
@@ -27,6 +28,7 @@ class QNetwork(nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
         # x = torch.relu(self.fc4(x))
         # x = torch.relu(self.fc5(x))
         # x = torch.relu(self.fc6(x))
